6015cd4f13c3080af2d36d6e62fac0c299635785
ZE-FRE <1757943893@qq.com>
2024-09-27 10:02:15 +0000

    add Semaphore.cpp.
    Signed-off-by: ZE-FRE <1757943893@qq.com>

---
+++ Semaphore.cpp
@@ -0,0 +1,22 @@
+#include "Semaphore.h"
+#include <stdlib.h>
+
+void P(Semaphore &semaphore)
+{
+	sem_wait(&semaphore.m_semaphore);
+}
+
+void V(Semaphore &semaphore)
+{
+	sem_post(&semaphore.m_semaphore);
+}
+
+Semaphore::Semaphore(int permits) :m_semaphore(nullptr) 
+{
+	sem_init(&m_semaphore, 0, permits);
+}
+
+Semaphore::~Semaphore()
+{
+	sem_destroy(&m_semaphore);
+}
